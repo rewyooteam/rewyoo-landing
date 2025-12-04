6bac4a03ab18d1d60dd43047d6d8f6f83caa6fbf
PradeepKundekar0101 <pradeepkundekargithub@gmail.com>
2025-12-04 20:06:27 +0530

    Update Prisma configuration to use PostgreSQL, downgrade Prisma dependencies to version 6.19.0, and clean up unused packages in package-lock.json.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,10 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+import { PrismaClient } from "../generated/prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db =
-  globalThis.prisma ??
-  new PrismaClient({
-    adapter,
-  });
+export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = db;
