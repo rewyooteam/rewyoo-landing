a55d80813fe8862cb16bbeaeef2c3fa8efb5b3b7
Vishal <vishalharade5503@gmail.com>
2025-12-04 20:09:05 +0530

    did it with prisma 7 it is working

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,18 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db =
-  globalThis.prisma ??
-  new PrismaClient({
-    adapter,
-  });
+export const db = globalThis.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = db;
